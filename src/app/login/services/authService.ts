8efb3175e56be409115ac166b40249bc2fd59ee7
Rick Kessler <rkessler@microsoft.com>
2020-01-09 13:25:13 -0800

    Add authService changes.

--- src/app/login/services/authService.ts
+++ src/app/login/services/authService.ts
@@ -4,26 +4,52 @@ let authResponse: Msal.AuthResponse | undefined;
 let authError: Msal.AuthError | undefined;
 
 const config: Msal.Configuration = {
-  auth: {
-    authority: 'https://login.microsoftonline.com/common',
-    clientId: 'c9d0ec1d-3630-477b-ae58-83585222d684',
-    redirectUri: window.location.origin
-  },
-  cache: {
-    storeAuthStateInCookie: true
-  }
+    auth: {
+        authority: 'https://login.microsoftonline.com/common',
+        clientId: 'c9d0ec1d-3630-477b-ae58-83585222d684',
+        redirectUri: window.location.origin
+    },
+    cache: {
+        storeAuthStateInCookie: true
+    }
 };
 
 const authenticationParameters: Msal.AuthenticationParameters = {
-  scopes: [
-    'openid',
-    'profile',
-    'User.read'
-  ]
+    scopes: [
+        'openid',
+        'profile',
+        'User.read'
+    ]
+};
+
+const azureResourceManagementParameters: Msal.AuthenticationParameters = {
+    scopes: [
+        'https://management.azure.com//user_impersonation',
+    ]
 };
 
 export const executeLoginRedirect = () => {
-    msalInstance.loginRedirect(authenticationParameters);
+      msalInstance.loginRedirect(authenticationParameters);
+};
+
+export const executeAzureResourceManagementTokenRequest = async (): Promise<string | undefined> => {
+
+    // tslint:disable-next-line:no-console
+    console.log('execute arm token request');
+
+    try {
+        const response = await msalInstance.acquireTokenSilent(azureResourceManagementParameters);
+        // tslint:disable-next-line:no-console
+        console.log('token!!!!' + response.accessToken);
+        return response.accessToken;
+    } catch (error) {
+        // tslint:disable-next-line:no-console
+        console.log('err' + JSON.stringify(error));
+
+        if (error.errorMessage && error.errorMessage.indexOf('interaction_required') !== -1) {
+            msalInstance.acquireTokenRedirect(azureResourceManagementParameters);
+        }
+    }
 };
 
 export const executeLogout = () => {
@@ -31,11 +57,11 @@ export const executeLogout = () => {
 };
 
 export const getLoginRedirectResponse = (): Msal.AuthResponse | undefined => {
-  return authResponse;
+    return authResponse;
 };
 
 export const getLoginRedirectError = (): Msal.AuthError | undefined => {
-  return authError;
+    return authError;
 };
 
 export const executeLoginRedirectCallback = (error: Msal.AuthError, response: Msal.AuthResponse) => {
